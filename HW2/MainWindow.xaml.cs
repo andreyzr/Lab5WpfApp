3f98d2d0299cdf087cd74ff47fc0340b64ae2666
andreyzr43 <andreyzr43@yandex.ru>
2023-05-31 10:44:26 +0300

    Доработка ДЗ

--- HW2/MainWindow.xaml.cs
+++ HW2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +26,92 @@ namespace HW2
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файл isf (*.isf)|*.isf";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(openFileDialog.FileName,
+                                               FileMode.Open);
+                Canvas.Strokes = new StrokeCollection(fs);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Файл isf (*.isf)|*.isf";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(saveFileDialog.FileName,
+                                 FileMode.Create);
+                Canvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string thickness = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+
+            switch (thickness)
+            {
+                case "Нормальная":
+                    Canvas.DefaultDrawingAttributes.Width = 5;
+                    break;
+                case "Жирная":
+                    Canvas.DefaultDrawingAttributes.Width = 10;
+                    break;
+                case "Тонкая":
+                    Canvas.DefaultDrawingAttributes.Width = 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Canvas.DefaultDrawingAttributes.Color != Colors.Red)
+            {
+                Canvas.DefaultDrawingAttributes.Color = Colors.Red;
+            }
+            else
+            {
+                Canvas.DefaultDrawingAttributes.Color = Colors.Black;
+            }
+
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Canvas.DefaultDrawingAttributes.Color != Colors.Green)
+            {
+                Canvas.DefaultDrawingAttributes.Color = Colors.Green;
+            }
+            else
+            {
+                Canvas.DefaultDrawingAttributes.Color = Colors.Black;
+            }
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Canvas.DefaultDrawingAttributes.Color != Colors.Blue)
+            {
+                Canvas.DefaultDrawingAttributes.Color = Colors.Blue;
+            }
+            else
+            {
+                Canvas.DefaultDrawingAttributes.Color = Colors.Black;
+            }
         }
     }
 }
